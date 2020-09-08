1f83e1104b3f7802870be75371a7e2b4c7bb3eda
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-09-08 12:09:06 +0200

    chore: configure purgecss for production builds

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
-module.exports = {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
+const purgecss = [
+    "@fullhuman/postcss-purgecss",
+    {
+      content: ["./components/**/*.js[x]?", "./pages/**/*.js[x]?", "./components/**/*.ts[x]?", "./pages/**/*.ts[x]?"],
+      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
     },
-};
+  ];
+  
+  module.exports = {
+    plugins: [
+      "postcss-import",
+      "tailwindcss",
+      "autoprefixer",
+      ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+    ],
+  };
