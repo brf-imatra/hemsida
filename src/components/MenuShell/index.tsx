363079d6f4aeb65d48b1d95a9b10a29c06ff2c68
Marcus Holmgren <marcus.holmgren1@gmail.com>
2020-09-09 16:46:21 +0200

    refactor: break out menu shell and menu item.

---
+++ src/components/MenuShell/index.tsx
@@ -0,0 +1,254 @@
+import React, { PropsWithChildren } from "react";
+import { Transition } from "@tailwindui/react";
+import { MenuItem, MenuItemIcon } from "..";
+
+interface MenuShellProps {
+  isShowing: boolean;
+  setIsShowing: (showing: boolean) => void;
+}
+
+export function MenuShell(props: PropsWithChildren<MenuShellProps>) {
+  const { isShowing, setIsShowing, children } = props;
+
+  const profileElement = false ? (
+    <div className="flex-shrink-0 flex border-t border-gray-200 p-4">
+      <a href="#" className="flex-shrink-0 w-full group block">
+        <div className="flex items-center">
+          <div>
+            <img
+              className="inline-block h-9 w-9 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </div>
+          <div className="ml-3">
+            <p className="text-sm leading-5 font-medium text-gray-700 group-hover:text-gray-900">
+              Tom Cook 1
+            </p>
+            <p className="text-xs leading-4 font-medium text-gray-500 group-hover:text-gray-700 transition ease-in-out duration-150">
+              View profile
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+  ) : null;
+
+  const mobileProfileElement = false ? (
+    <div className="flex-shrink-0 flex border-t border-grey-200 p-4 bg-red-600">
+      <a href="#" className="flex-shrink-0 group block focus:outline-none">
+        <div className="flex items-center">
+          <div>
+            <img
+              className="inline-block h-10 w-10 rounded-full"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </div>
+          <div className="ml-3">
+            <p className="text-base leading-6 font-medium text-gray-700 group-hover:text-gray-900">
+              Tom Cook
+            </p>
+            <p className="text-sm leading-5 font-medium text-gray-500 group-hover:text-gray-700 group-focus:underline transition ease-in-out duration-150">
+              View profile
+            </p>
+          </div>
+        </div>
+      </a>
+    </div>
+  ) : null;
+
+  return (
+    <>
+      <div className="h-screen flex overflow-hidden bg-white">
+        <Transition show={isShowing}>
+          <div className="md:hidden">
+            <div className="fixed inset-0 flex z-40">
+              <Transition.Child
+                enter="transition-opacity ease-linear duration-300"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity ease-linear duration-300"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                {(ref) => (
+                  <div ref={ref} className="fixed inset-0">
+                    <div
+                      onClick={() => setIsShowing(false)}
+                      className="absolute inset-0 opacity-75 bg-cool-gray-600"
+                    />
+                  </div>
+                )}
+              </Transition.Child>
+
+              <Transition.Child
+                appear={true}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                {(ref) => (
+                  <div ref={ref}>
+                    <div className="relative flex-1 flex flex-col max-w-xs w-full bg-white">
+                      <div className="absolute top-0 right-0 -mr-14 p-1">
+                        {/* start */}
+                        <div
+                          className="absolute top-0 right-0 -mr-14 p-1"
+                          onClick={() => setIsShowing(false)}
+                        >
+                          <Transition.Child
+                            className="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600"
+                            aria-label="Close sidebar"
+                            as="button"
+                            onClick={() => setIsShowing(false)}
+                          >
+                            <svg
+                              className="h-6 w-6 text-gray-600 active:text-white"
+                              stroke="currentColor"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M6 18L18 6M6 6l12 12"
+                              />
+                            </svg>
+                          </Transition.Child>
+                        </div>
+                        {/* end */}
+                      </div>
+                      <div className="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
+                        <div className="flex-shrink-0 flex items-center px-4">
+                          <img
+                            className="h-8 w-auto"
+                            src="/android-chrome-192x192.png"
+                            alt="Brf Imatra"
+                          />
+                        </div>
+                        <nav className="mt-5 px-2 space-y-1">
+                          <MenuItem
+                            text="Hem"
+                            href="/"
+                            mobile={true}
+                            icon={MenuItemIcon.home}
+                          />
+                          <MenuItem
+                            text="Medlemsinformation"
+                            mobile={true}
+                            icon={MenuItemIcon.users}
+                          />
+                          <MenuItem
+                            text="Kontakta oss"
+                            mobile={true}
+                            icon={MenuItemIcon.folder}
+                          />
+                          <MenuItem
+                            text="Föreningssträmma"
+                            mobile={true}
+                            icon={MenuItemIcon.scheduel}
+                          />
+                          <MenuItem
+                            text="Dokument"
+                            mobile={true}
+                            icon={MenuItemIcon.document}
+                          />
+                          <MenuItem
+                            text="Nyhetsarkiv"
+                            mobile={true}
+                            icon={MenuItemIcon.barchart}
+                          />
+                        </nav>
+                      </div>
+                      {mobileProfileElement}
+                    </div>
+
+                    <div className="flex-shrink-0 w-14"></div>
+                  </div>
+                )}
+              </Transition.Child>
+            </div>
+          </div>
+        </Transition>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden md:flex md:flex-shrink-0">
+          <div className="flex flex-col w-64">
+            <div className="flex flex-col h-0 flex-1 border-r border-gray-200 bg-white">
+              <div className="flex-1 flex flex-col pt-5 pb-4 overflow-y-auto">
+                <div className="flex items-center flex-shrink-0 px-4">
+                  <img
+                    className="h-8 w-auto"
+                    src="/android-chrome-192x192.png"
+                    alt="Workflow"
+                  />
+                </div>
+                <nav className="mt-5 flex-1 px-2 bg-white space-y-1">
+                  <MenuItem text="Hem" href="/" icon={MenuItemIcon.home} />
+                  <MenuItem
+                    text="Medlemsinformation"
+                    icon={MenuItemIcon.users}
+                  />
+                  <MenuItem text="Kontakta oss" icon={MenuItemIcon.folder} />
+                  <MenuItem
+                    text="Föreningssträmma"
+                    icon={MenuItemIcon.scheduel}
+                  />
+                  <MenuItem text="Dokument" icon={MenuItemIcon.document} />
+                  <MenuItem text="Nyhetsarkiv" icon={MenuItemIcon.barchart} />
+                </nav>
+              </div>
+              {profileElement}
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col w-0 flex-1 overflow-hidden" tabIndex={0}>
+          <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
+            <button
+              className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:bg-gray-200 transition ease-in-out duration-150"
+              aria-label="Open sidebar"
+              onClick={() => setIsShowing(true)}
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+          </div>
+          <main
+            className="flex-1 relative z-0 overflow-y-auto focus:outline-none"
+            tabIndex={0}
+          >
+            <div className="pt-2 pb-6 md:py-6">
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <h1 className="text-2xl font-semibold text-gray-900">
+                  Information om BRF Imatra
+                </h1>
+              </div>
+              <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <div className="py-4">
+                  <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+                </div>
+                {children}
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,49 @@
-import { AppProps } from 'next/app'
-import '../css/tailwind.css'
-import Head from 'next/head'
+import { AppProps } from "next/app";
+import "../css/tailwind.css";
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { MenuShell } from "../components";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isShowing, setIsShowing] = useState(false);
+
+  useEffect(() => {
+    function handleEscape(event) {
+      if (!isShowing) return;
+
+      if (event.key === "Escape") {
+        setIsShowing(false);
+      }
+    }
+
+    document.addEventListener("keyup", handleEscape);
+    return () => document.removeEventListener("keyup", handleEscape);
+  }, [isShowing]);
+
   return (
     <>
       <Head>
         <title>BRF Imatra - Hemsida</title>
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-chrome-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="512x512"
+          href="/android-chrome-512x512.png"
+        />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel='manifest' href='/site.webmanifest' />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
-      <Component {...pageProps} />
+      <MenuShell isShowing={isShowing} setIsShowing={setIsShowing.bind(this)}>
+        <Component {...pageProps} />
+      </MenuShell>
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
