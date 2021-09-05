7ffea8174db3c1cef42a6bdc3477d5b26a6eead3
Marcus Holmgren <marcus.holmgren1@gmail.com>
2021-09-05 17:14:03 +0200

    feat: upgrade to Tailwind CSS 2 🎉

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   purge: [
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/pages/**/*.{js,ts,jsx,tsx}',
