6db1ef48cb6888279f462bcc058e9f7f6c63c968
Marcus Holmgren <marcus.holmgren1@gmail.com>
2022-11-12 15:57:05 +0100

    Nya ImatraNytt för november/december 2022 (#46)

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -14,23 +14,23 @@ export function Card(props: CardProps) {
   return (
     <div className="bg-white border border-lg rounded-lg overflow-hidden">
       <Link href={props.hrefLink}>
-          <img
-            src={props.imageUrl}
-            alt={props.imageAlt}
-            className="max-w-md h-40"
-            loading="lazy"
-          />
-          <div className="p-6">
-            <h4 className="font-semibold text-lg leading-tight truncate">
-              {props.title}
-            </h4>
-            <div className="text-gray-600 text-xs uppercase font-medium tracking-wide mt-2">
-              {props.published}
-            </div>
-            <div className="mt-6">
-              <span className="text-gray-600 text-sm">{props.summary}</span>
-            </div>
+        <img
+          src={props.imageUrl}
+          alt={props.imageAlt}
+          className="max-w-md h-40"
+          loading="lazy"
+        />
+        <div className="p-6">
+          <h4 className="font-semibold text-lg leading-tight truncate">
+            {props.title}
+          </h4>
+          <div className="text-gray-600 text-xs uppercase font-medium tracking-wide mt-2">
+            {props.published}
           </div>
+          <div className="mt-6">
+            <span className="text-gray-600 text-sm">{props.summary}</span>
+          </div>
+        </div>
       </Link>
     </div>
   );
