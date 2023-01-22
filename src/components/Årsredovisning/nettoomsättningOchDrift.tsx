d1a305a15bc62e46c84baec993ea80e03a6c10d1
Marcus Holmgren <marcus.holmgren1@gmail.com>
2023-01-22 16:39:36 +0100

    ImatraNytt 2023

--- src/components/Årsredovisning/nettoomsättningOchDrift.tsx
+++ src/components/Årsredovisning/nettoomsättningOchDrift.tsx
@@ -1,4 +1,4 @@
-import { Card, Title, AreaChart } from "@tremor/react";
+import { Card, Title, Text, AreaChart } from '@tremor/react';
 
 /*
 Fastighetsförsäkring
@@ -29,112 +29,110 @@ Fastighetsförsäkring
 // Not 2 från årsredovisningarna
 
 const fastighetsFörsäkringsData = [
-    {
-    date: "2001",
-    "Nettoomsättning": 12_785_128,
-              "Drift och underhåll": 6_312_944,
-  },
-      {
-    date: "2002",
-    "Nettoomsättning": 12_965_748,
-              "Drift och underhåll": 6_586_615,
-  },
-      {
-    date: "2003",
-    "Nettoomsättning": 12_928_491,
-              "Drift och underhåll": 7_506_577,
-  },
-                  {
-    date: "2004",
-    "Nettoomsättning": 13_017_777,
-              "Drift och underhåll": 7_596_008,
-  },
-                  {
-    date: "2005",
-    "Nettoomsättning": 13_081_833,
-              "Drift och underhåll": 8_084_553,
-  },
-                  {
-    date: "2006",
-    "Nettoomsättning": 13_014_976,
-              "Drift och underhåll": 8_056_265,
-  },
-              {
-    date: "2007",
-    "Nettoomsättning": 12_967_828,
-              "Drift och underhåll": 8_506_810,
-  },
-              {
-    date: "2008",
-    "Nettoomsättning": 12_886_694,
-              "Drift och underhåll": 9_326_738,
-  },
-              {
-    date: "2009",
-    "Nettoomsättning": 13_168_125,
-              "Drift och underhåll": 9_616_649,
-  },
-          {
-    date: "2010",
-    "Nettoomsättning": 13_459_014,
-              "Drift och underhåll": 10_299_298,
-  },
-          {
-    date: "2011",
-    "Nettoomsättning": 13_537_271,
-              "Drift och underhåll": 10_109_505,
-  },
-              {
-    date: "2012",
-    "Nettoomsättning": 13_543_369,
-                  "Drift och underhåll": 11_131_403
-  },
-      {
-    date: "2013",
-    "Nettoomsättning": 13_988_214,
-          "Drift och underhåll": 10_126_183
-  },
-      {
-    date: "2014",
-    "Nettoomsättning": 14_608_423,
-          "Drift och underhåll": 11_885_166,
-  },
-      {
-    date: "2015",
-    "Nettoomsättning": 14_908_998,
-          "Drift och underhåll": 11_004_212,
-  },
-      {
-    date: "2016",
-    "Nettoomsättning": 14_854_676,
-          "Drift och underhåll": 12_616_564,
-  },
-      {
-    date: "2017",
-    "Nettoomsättning": 15_600_662,
-          "Drift och underhåll": 13_367_397,
+  {
+    date: '2001',
+    Nettoomsättning: 12_785_128,
+    'Drift och underhåll': 6_312_944,
   },
   {
-    date: "2018",
-    "Nettoomsättning": 16_036_867,
-      "Drift och underhåll": 12_676_863,
+    date: '2002',
+    Nettoomsättning: 12_965_748,
+    'Drift och underhåll': 6_586_615,
   },
   {
-    date: "2019",
-    "Nettoomsättning": 16_528_380,
-      "Drift och underhåll": 15_045_455,
+    date: '2003',
+    Nettoomsättning: 12_928_491,
+    'Drift och underhåll': 7_506_577,
   },
   {
-    date: "2020",
-    "Nettoomsättning": 16_736_458,
-      "Drift och underhåll": 13_272_554,
-
+    date: '2004',
+    Nettoomsättning: 13_017_777,
+    'Drift och underhåll': 7_596_008,
   },
   {
-    date: "2021",
-    "Nettoomsättning": 17_169_064,
-      "Drift och underhåll": 18_594_590,
-
+    date: '2005',
+    Nettoomsättning: 13_081_833,
+    'Drift och underhåll': 8_084_553,
+  },
+  {
+    date: '2006',
+    Nettoomsättning: 13_014_976,
+    'Drift och underhåll': 8_056_265,
+  },
+  {
+    date: '2007',
+    Nettoomsättning: 12_967_828,
+    'Drift och underhåll': 8_506_810,
+  },
+  {
+    date: '2008',
+    Nettoomsättning: 12_886_694,
+    'Drift och underhåll': 9_326_738,
+  },
+  {
+    date: '2009',
+    Nettoomsättning: 13_168_125,
+    'Drift och underhåll': 9_616_649,
+  },
+  {
+    date: '2010',
+    Nettoomsättning: 13_459_014,
+    'Drift och underhåll': 10_299_298,
+  },
+  {
+    date: '2011',
+    Nettoomsättning: 13_537_271,
+    'Drift och underhåll': 10_109_505,
+  },
+  {
+    date: '2012',
+    Nettoomsättning: 13_543_369,
+    'Drift och underhåll': 11_131_403,
+  },
+  {
+    date: '2013',
+    Nettoomsättning: 13_988_214,
+    'Drift och underhåll': 10_126_183,
+  },
+  {
+    date: '2014',
+    Nettoomsättning: 14_608_423,
+    'Drift och underhåll': 11_885_166,
+  },
+  {
+    date: '2015',
+    Nettoomsättning: 14_908_998,
+    'Drift och underhåll': 11_004_212,
+  },
+  {
+    date: '2016',
+    Nettoomsättning: 14_854_676,
+    'Drift och underhåll': 12_616_564,
+  },
+  {
+    date: '2017',
+    Nettoomsättning: 15_600_662,
+    'Drift och underhåll': 13_367_397,
+  },
+  {
+    date: '2018',
+    Nettoomsättning: 16_036_867,
+    'Drift och underhåll': 12_676_863,
+  },
+  {
+    date: '2019',
+    Nettoomsättning: 16_528_380,
+    'Drift och underhåll': 15_045_455,
+  },
+  {
+    date: '2020',
+    Nettoomsättning: 16_736_458,
+    'Drift och underhåll': 13_272_554,
+  },
+  {
+    date: '2021',
+    Nettoomsättning: 17_169_064,
+    'Drift och underhåll': 18_594_590,
   },
   // {
   //   date: "2022",
@@ -149,23 +147,24 @@ const fastighetsFörsäkringsData = [
 ];
 
 const dataFormatter = (number: number) => {
-  return Intl.NumberFormat("sv").format(number).toString() + " kr";
+  return Intl.NumberFormat('sv').format(number).toString() + ' kr';
 };
 
 export function NettoomsättningOchDrift() {
-    return (
-  <Card>
-    <Title>Nettoomsättning samt drift och underhåll</Title>
-    <AreaChart
-      data={fastighetsFörsäkringsData}
-      categories={["Nettoomsättning", "Drift och underhåll"]}
-      dataKey="date"
-      height="h-72"
-      colors={["amber", 'pink']}
-      yAxisWidth={"w-24"}
-      valueFormatter={dataFormatter}
-      marginTop="mt-4"
-    />
-  </Card>
-);
+  return (
+    <Card>
+      <Title>Nettoomsättning samt drift och underhåll</Title>
+      <Text>Jämförelse av nettoomsättning mot kostnaderna för drift och underhåll</Text>
+      <AreaChart
+        data={fastighetsFörsäkringsData}
+        categories={['Nettoomsättning', 'Drift och underhåll']}
+        dataKey="date"
+        height="h-72"
+        colors={['amber', 'pink']}
+        yAxisWidth={'w-24'}
+        valueFormatter={dataFormatter}
+        marginTop="mt-4"
+      />
+    </Card>
+  );
 }
--- src/pages/boendeinformation/stambyte/faq.tsx
+++ src/pages/boendeinformation/stambyte/faq.tsx
@@ -1,29 +1,30 @@
-import { Disclosure } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/24/outline'
-import Link from "next/link";
+import { Disclosure } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 type FAQ = {
-    question: string
-    answer: string
-    link?: string
-}
+  question: string;
+  answer: string;
+  link?: string;
+};
 
-const faqs : Array<FAQ> = [
+const faqs: Array<FAQ> = [
   {
     question: 'Vattenburen eller el-uppvärmt golvvärme?',
-    answer: 'Styrelsen beställde en analys för att kunna fatta ett informerat beslut gällande golvvärme i badrum. Utifrån rapporten har styrelsen beslut att el-uppvärmda badrumsgolv kommer att installeras. Eftersom det handlar om komfortvärme och inte uppvärmning av badrummet gör det svårt att motivera den högre installationskostnaden för vattenburengolvvärme. Det el-uppvärmda badrumsgolvet kommer att gå att stänga av för den som vill spara in på sin elräkningen.',
-    link: '/boendeinformation/stambyte/rapport-golvvarme'
+    answer:
+      'Styrelsen beställde en analys för att kunna fatta ett informerat beslut gällande golvvärme i badrum. Utifrån rapporten har styrelsen beslut att el-uppvärmda badrumsgolv kommer att installeras. Eftersom det handlar om komfortvärme och inte uppvärmning av badrummet gör det svårt att motivera den högre installationskostnaden för vattenburengolvvärme. Det el-uppvärmda badrumsgolvet kommer att gå att stänga av för den som vill spara in på sin elräkningen.',
+    link: '/boendeinformation/stambyte/rapport-golvvarme',
   },
   {
-    question: "När beräknas stambytet påbörjas?",
+    question: 'När beräknas stambytet påbörjas?',
     answer:
-      "Styrelsen kan inte ge något exakt datum för när stambytesarbetet kommer att påbörjas. Upphandlingsunderlag är skickat till fyra entreprenörer och styrelsen jobbar för att kunna påbörja stambyte under april 2023. Vi kommer att hålla er uppdaterade om hur det går med stambytet.",
+      'Styrelsen kan inte ge något exakt datum för när stambytesarbetet kommer att påbörjas. Upphandlingsunderlag är skickat till fyra entreprenörer och styrelsen jobbar för att kunna påbörja stambyte under april 2023. Vi kommer att hålla er uppdaterade om hur det går med stambytet.',
   },
   // More questions...
-]
+];
 
 function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(' ');
 }
 
 export default function StambyteFaq() {
@@ -41,20 +42,27 @@ export default function StambyteFaq() {
                   <>
                     <dt className="text-lg">
                       <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
-                        <span className="font-medium text-gray-900">{faq.question}</span>
+                        <span className="font-medium text-gray-900">
+                          {faq.question}
+                        </span>
                         <span className="ml-6 flex h-7 items-center">
                           <ChevronDownIcon
-                            className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
+                            className={classNames(
+                              open ? '-rotate-180' : 'rotate-0',
+                              'h-6 w-6 transform',
+                            )}
                             aria-hidden="true"
                           />
                         </span>
                       </Disclosure.Button>
                     </dt>
                     <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                        <>
-                      <p className="text-base text-gray-500">{faq.answer}</p>
-                      {faq.link ? <Link href={faq.link!}>Läs mer...</Link> : null}
-                        </>
+                      <>
+                        <p className="text-base text-gray-500">{faq.answer}</p>
+                        {faq.link ? (
+                          <Link href={faq.link!}>Läs mer...</Link>
+                        ) : null}
+                      </>
                     </Disclosure.Panel>
                   </>
                 )}
@@ -64,5 +72,5 @@ export default function StambyteFaq() {
         </div>
       </div>
     </div>
-  )
+  );
 }
