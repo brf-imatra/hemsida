91b28151181d98b5462291b28fc52efb3818f32f
Marcus Holmgren <marcus.holmgren1@gmail.com>
2023-09-02 18:19:01 +0200

    Share button on homepage

--- src/components/MenuShell/footer.tsx
+++ src/components/MenuShell/footer.tsx
@@ -37,6 +37,7 @@ export default function SocialLinksFooter() {
               <item.icon className='h-6 w-6' aria-hidden='true' />
             </a>
           ))}
+          <ShareButton />
         </div>
         <div className='mt-8 md:order-1 md:mt-0'>
           <p className='text-center text-base text-gray-400'>
@@ -45,5 +46,37 @@ export default function SocialLinksFooter() {
         </div>
       </div>
     </footer>
+  );
+}
+
+
+function ShareButton() {
+
+  const share = async () => {
+    if (navigator.share) {
+      try {
+        await navigator.share({
+          title: 'Brf Imatra',
+          text: 'HSB Brf Imatra i Stockholm',
+          url: 'https://brfimatra.se',
+        });
+        console.log('Data was shared successfully');
+      } catch (err) {
+        console.error('Share failed:', err.message);
+      }
+    } else {
+      console.log('navigator.share() not supported.');
+    }
+  };
+
+  return (
+    <button className='text-gray-400 hover:text-gray-500' onClick={share}>
+      <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor'
+           className='w-6 h-6'>
+        <title>Dela brf Imatra hemsida</title>
+        <path strokeLinecap='round' strokeLinejoin='round'
+              d='M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z' />
+      </svg>
+    </button>
   );
 }
